dbd9ce5e82c412b616eb1ce27d9cf9c87d94b649
HungPham <hungpham0307@gmail.com>
2021-10-18 23:06:05 +0700

    Add Unit Test

--- app/config/config.php
+++ app/config/config.php
@@ -4,3 +4,4 @@ define('DB_USER_NAME', 'root');
 define('DB_PASSWORD', '1234');
 define('DB_NAME', 'lists');
 define('DB_CHARSET', 'utf8');
+define('APP_URL', 'http://todolist.local');
--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-require_once "./app/vendor/autoload.php";
+require_once __DIR__ . "/vendor/autoload.php";
+
+require_once __DIR__ . '/app/config/config.php';
+require_once __DIR__ . '/app/enums/Enums.php';
+require_once __DIR__ . '/app/enums/WorkStatus.php';
+require_once __DIR__ . '/app/traits/ConvertString.php';
+require_once __DIR__ . '/app/connection/Database.php';
+require_once __DIR__ . '/app/models/Model.php';
+require_once __DIR__ . "/app/core/App.php";
+
+new App\Core\App;
